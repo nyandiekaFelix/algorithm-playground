50b2ee1ff95a0adbad0b7f00bc5efe5a98047580
nyandiekaFelix <nyandiekafelix@gmail.com>
2017-02-12 18:06:22 +0300

    update solution

---
+++ vowel-count/solution.js
@@ -0,0 +1,16 @@
+function getCount(str) {
+  let vowelsCount = 0;
+  const vowels = 'aeiou';
+  
+  // Iterate Over Given String
+  for (var letter = 0; letter < str.length; letter++) {
+
+    // Check the string for an occurence of a vowel and update the count 
+    if (vowels.indexOf(str[letter]) > -1){
+      vowelsCount++;
+    }
+  }
+  // enter your majic here
+  
+  return vowelsCount;
+}
